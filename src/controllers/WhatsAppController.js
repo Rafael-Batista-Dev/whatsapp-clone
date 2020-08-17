c5c2472db6b35df019a0433d3ab5163e8c33a0ce
unknown <rafaelbatista.sth@gmail.com>
2020-08-17 17:59:36 -0300

    events in the "Type" field message"

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -226,8 +226,51 @@ class WhatsAppController {
     this.el.btnFinishMicrophone.on("click", (e) => {
       this.closeRecordMicrophone();
     });
-
     //================MicroPhone===================
+
+    //================inputText===================
+
+    //Saber qual tecla precionou
+    this.el.inputText.on("keypress", (e) => {
+      if (e.key === "Enter" && !e.ctrlKey) {
+        e.preventDefault();
+
+        this.el.btnSend.click();
+      }
+    });
+
+    this.el.inputText.on("keyup", (e) => {
+      if (this.el.inputText.innerHTML.length) {
+        this.el.inputPlaceholder.hide();
+        this.el.btnSendMicrophone.hide();
+        this.el.btnSend.show();
+      } else {
+        this.el.inputPlaceholder.show();
+        this.el.btnSendMicrophone.show();
+        this.el.btnSend.hide();
+      }
+    });
+
+    //Configurando Emojis
+    this.el.btnEmojis.on("click", (e) => {
+      this.el.panelEmojis.toggleClass("open");
+    });
+
+    //Pegando click do emoji
+    this.el.panelEmojis.querySelectorAll(".emojik").forEach((emoji) => {
+      emoji.on("click", (e) => {
+        console.log(emoji.dataset.unicode);
+      });
+    });
+
+    this.el.btnSend.on("click", (e) => {
+      console.log(this.el.inputText.innerHTML);
+    });
+    //================inputText===================
+
+    //
+    //
+    //
   }
 
   //metodo para iniciar microphone

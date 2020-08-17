eb5aa19203ff545c209392a0b96927ca31027977
unknown <rafaelbatista.sth@gmail.com>
2020-08-17 12:38:06 -0300

    events to open / close "edit profile" panels add contact

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController {
 
     this.elementsPrototype();
     this.loadElements();
+    this.initEvent();
   }
 
   loadElements() {
@@ -62,5 +63,36 @@ class WhatsAppController {
     Element.prototype.hasClass = function (name) {
       return this.classList.contains(name);
     };
+  }
+
+  initEvent() {
+    this.el.myPhoto.on("click", (e) => {
+      this.closeAllLeftPanel();
+      this.el.panelEditProfile.show();
+      setTimeout(() => {
+        this.el.panelEditProfile.addClass("open");
+      }, 300);
+    });
+
+    this.el.btnNewContact.on("click", (e) => {
+      this.closeAllLeftPanel();
+      this.el.panelAddContact.show();
+      setTimeout(() => {
+        this.el.panelAddContact.addClass("open");
+      }, 300);
+    });
+
+    this.el.btnClosePanelEditProfile.on("click", (e) => {
+      this.el.panelEditProfile.removeClass("open");
+    });
+
+    this.el.btnClosePanelAddContact.on("click", (e) => {
+      this.el.panelAddContact.removeClass("open");
+    });
+  }
+
+  closeAllLeftPanel() {
+    this.el.panelAddContact.hide();
+    this.el.panelEditProfile.hide();
   }
 }

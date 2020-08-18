c34ff9625ac77962cc88721dd041a2ccfc196be4
unknown <rafaelbatista.sth@gmail.com>
2020-08-18 09:27:47 -0300

    stop teke picture

--- src/controllers/CameraController.js
+++ src/controllers/CameraController.js
@@ -7,18 +7,23 @@ export class CameraController {
         video: true,
       })
       .then((stream) => {
-        /*
-        
+        /*  
+        this._videoEl.srcObject = stream;
+        this._videoEl.play();  
+        */
+
         this._stream = stream;
         this._videoEl.src = URL.createObjectURL(stream);
-        this._videoEl.play();
-        
-        */
-        this._videoEl.srcObject = stream;
         this._videoEl.play();
       })
       .catch((err) => {
         console.error(err);
       });
+  }
+
+  stop() {
+    this._stream.stream.getTracks().forEach((track) => {
+      track.stop();
+    });
   }
 }

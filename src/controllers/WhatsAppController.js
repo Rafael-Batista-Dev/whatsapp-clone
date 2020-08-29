dc5da1f1adc32c421954ed0542d668af140622e3
unknown <rafaelbatista.sth@gmail.com>
2020-08-29 11:01:31 -0300

    Take photo from camera with canvas

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -184,7 +184,29 @@ export class WhatsAppController {
     });
 
     this.el.btnTakePicture.on("click", (e) => {
-      console.log("Teke Picture");
+      //recebendo o method takePicture
+      let dataUrl = this._camera.takePicture();
+
+      this.el.pictureCamera.src = dataUrl;
+      this.el.pictureCamera.show();
+      this.el.videoCamera.hide();
+      this.el.btnReshootPanelCamera.show();
+      this.el.containerTakePicture.hide();
+      this.el.containerSendPicture.show();
+
+      //console.log("picture");
+    });
+
+    this.el.btnSendPicture.on("click", (e) => {
+      console.log(this.el.pictureCamera.src);
+    });
+
+    this.el.btnReshootPanelCamera.on("click", (e) => {
+      this.el.pictureCamera.hide();
+      this.el.videoCamera.show();
+      this.el.btnReshootPanelCamera.hide();
+      this.el.containerTakePicture.show();
+      this.el.containerSendPicture.hide();
     });
 
     //===================Camera====================

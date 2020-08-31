5d6f81998b79ed86efeae5fa5768fc9d74fd6a0a
unknown <rafaelbatista.sth@gmail.com>
2020-08-31 10:32:50 -0300

    firebase authentication google

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -8,10 +8,25 @@ export class WhatsAppController {
   constructor() {
     console.log("hello world");
 
+    this._firebase = new Firebase();
+    this.initAuth();
     this.elementsPrototype();
     this.loadElements();
     this.initEvent();
-    this._firebase = new Firebase();
+  }
+
+  initAuth() {
+    this._firebase
+      .initAuth()
+      .then((response) => {
+        this._user = response.user;
+        this.el.appContent.css({
+          display: "flex",
+        });
+      })
+      .catch((err) => {
+        console.error("err", err);
+      });
   }
 
   loadElements() {
